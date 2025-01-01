fe96308657b0fbf505e0ca19c555100bec776061
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2025-01-01 23:13:06 +0530

    Making our contract Ownable

--- src/MinimalAccount.sol
+++ src/MinimalAccount.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.24;
 
 import { IAccount } from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import { PackedUserOperation } from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
+
+    constructor() Ownable(msg.sender){}
 
     function validateUserOp(
         PackedUserOperation calldata userOp,
