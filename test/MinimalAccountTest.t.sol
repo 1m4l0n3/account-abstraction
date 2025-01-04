1d5b78d45a38f84ebe4e4b672dcd7a1ed2f90a18
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2025-01-05 01:07:44 +0530

    Implement a test case to check only owner can execute a command

---
+++ test/MinimalAccountTest.t.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test,console} from "../lib/forge-std/src/Test.sol";
+import {MinimalAccountDeploy} from "../script/MinimalAccountDeploy.s.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {MinimalAccount} from "../src/MinimalAccount.sol";
+import {ERC20Mock} from "../lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
+
+contract MinimalAccountTest is Test {
+    MinimalAccountDeploy private deployer;
+    MinimalAccount private minimalAccount;
+    HelperConfig public helperConfig;
+    ERC20Mock private usdc;
+
+    function setUp() external {
+        deployer = new MinimalAccountDeploy();
+        (helperConfig, minimalAccount) = deployer.deployMinimalAccount();
+        usdc = new ERC20Mock();
+    }
+
+    function testOwnerCanExecuteCommands() external {
+        assertEq(usdc.balanceOf(address(minimalAccount)),0);
+        address destination = address(usdc);
+        uint256 value = 0;
+        uint256 amount = 1e18;
+
+        bytes memory callData = abi.encodeWithSelector(ERC20Mock.mint.selector,address(minimalAccount),amount);
+
+        vm.prank(minimalAccount.owner());
+        minimalAccount.execute(destination,value,callData);
+
+        assertEq(usdc.balanceOf(address(minimalAccount)),amount);
+    }
+}
