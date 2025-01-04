f4464a47f87f1b7744411f68af226f6ef4a72118
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2025-01-04 15:59:14 +0530

    Implement logic for internal validateUserOp function

--- src/MinimalAccount.sol
+++ src/MinimalAccount.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.24;
 import { IAccount } from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import { PackedUserOperation } from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "../lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 
 contract MinimalAccount is IAccount, Ownable {
 
@@ -13,5 +16,16 @@ contract MinimalAccount is IAccount, Ownable {
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
-    ) external returns (uint256 validationData) {}
+    ) external returns (uint256 validationData) {
+        uint256 validationData = _validateUserOp(userOp,userOpHash);
+    }
+
+    function _validateUserOp(PackedUserOperation memory userOp,bytes32 userOpHash) internal view returns(uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash,userOp.signature);
+        if ( signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
 }
